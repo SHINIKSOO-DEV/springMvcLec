861bc881d7fdb7ffa311abf1b1aae3c89cce6348
soolism <soolism@daum.net>
2025-07-31 23:41:50 +0900

    get방식

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,33 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+
+/**
+ * 1. 파라미터 전송 기능
+ * http://localhost:8080/request-param?username=hello&age=20
+ */
+
+@WebServlet(name = "RequestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("RequestParamServlet.service");
+        req.getParameterNames().asIterator().forEachRemaining(paramName -> System.out.println(paramName + " = " + req.getParameter(paramName)));
+
+
+        System.out.println("[단일 파라미터조회]");
+        System.out.println("username = " + req.getParameter("username"));
+
+        System.out.println("[이름이 같은 복수 파라미터조회]");
+        Arrays.stream(req.getParameterValues("username")).iterator().forEachRemaining(System.out::println);
+    }
+}
+
